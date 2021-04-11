de688ba53028bb8fda26115610274812614976be
Yusuke Ohashi <github@junkpiano.me>
2021-04-11 17:21:34 +0900

    auto increment counts

--- count.sh
+++ count.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-find . | grep ".md" | grep -v "README.md" | wc -l
+COUNT=$(find . | grep ".md" | grep -v "README.md" | wc -l | xargs echo)
+
+sed -i '' -e "s/^\*[0-9]* TILs/\*${COUNT} TILs/" README.md
